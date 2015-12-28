d87599b8eedda47e33719ce624d1b8d93aa4292b
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-28 04:51:25 +0100

    add dart_dev tools and update dependencies

--- tool/dev.dart
+++ tool/dev.dart
@@ -3,16 +3,8 @@ library tool.dev;
 import 'package:dart_dev/dart_dev.dart' show dev, config;
 
 main(List<String> args) async {
-  List<String> directories = ['example/', 'lib/', 'test/', 'tool/'];
-
-  config.analyze.entryPoints = directories;
-  config.format.directories = directories;
-  config.test
-    ..platforms = ['vm']
-    ..pubServe = true
-    ..unitTests = ['test/'];
-
-  config.coverage.pubServe = true;
+  config.analyze.entryPoints = ['lib/', 'test/', 'tool/', 'example/'];
+  config.format.directories = ['lib/', 'test/', 'tool/', 'example/'];
 
   await dev(args);
 }
